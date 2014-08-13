a892ab9fbf94e8f00b3ffe83a2d2efc71fbc3bc9
Caleb Hattingh <caleb.hattingh@gmail.com>
2014-08-13 15:38:46 +1000

    Added a py27 compatible utility for downloading a file.  This is to bootstrap miniconda.

---
+++ appveyor/dlfile.py
@@ -0,0 +1,21 @@
+from __future__ import (print_function, division)
+
+import sys
+import urllib
+import urlparse
+
+
+def get_filename_from_url(url):
+    return urlparse.urlsplit(url).path.split('/')[-1]
+
+
+def main(url, filename):
+    print('Downloading {}...'.format(filename))
+    urllib.urlretrieve(url, filename=filename)
+    print('Done.')
+
+
+if __name__ == '__main__':
+    url = ''.join(sys.argv[1:])
+    filename = get_filename_from_url(url)
+    main(url, filename)
