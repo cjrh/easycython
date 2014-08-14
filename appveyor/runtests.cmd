a2a6a132f93577bd0f67b69bb228a3dbd46b4152
Caleb Hattingh <caleb.hattingh@gmail.com>
2014-08-14 23:10:49 +1000

    Added more ECHOs.

--- appveyor/runtests.cmd
+++ appveyor/runtests.cmd
@@ -1,6 +1,9 @@
 REM Augment the path so that the correct python gets used.
 SET PATH=%PYDIR%\envs\testenv;%PYDIR%\envs\testenv\Scripts;%PATH%
+ECHO Inside runtests.cmd
+ECHO This is the path:
 ECHO %PATH%
+ECHO python -V
 REM Run tests
 python setup.py develop
 py.test
