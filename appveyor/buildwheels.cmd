2cba79fa7fb4c303a3d32fee7e5783559b2e191e
Caleb Hattingh <caleb.hattingh@gmail.com>
2014-08-24 11:45:46 +1000

    Apparently setuptools is missing in the build script, leads me to think PYTHONPATH is messing things up again.

--- appveyor/buildwheels.cmd
+++ appveyor/buildwheels.cmd
@@ -1,4 +1,13 @@
 REM Augment the path so that the correct python gets used.
-SET PATH=%PYDIR%\envs\testenv;%PYDIR%\envs\testenv\Scripts;%PATH%
+SET PATHENV=%PYDIR%\envs\testenv
+SET PATH=%PATHENV%;%PATHENV%\Scripts;%PATH%
+SET PYTHONHOME=
+SET PYTHONPATH=
+:: Replicate cmd option /V:ON (delayed expansion - required)
+setlocal EnableDelayedExpansion
+CALL "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /Release /x64
+SET DISTUTILS_USE_SDK=1
+SET MSSdk=1
+ECHO ========================================================
 REM Make the wheel
-python setup.py bdist_wheel
+%PATHENV%\python.exe setup.py bdist_wheel
