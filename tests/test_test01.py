85ee8afb4cd232b6690cc1b6b3449f2e2694cfd6
Caleb Hattingh <caleb.hattingh@gmail.com>
2015-02-13 11:07:13 +1000

    Reverted testing file. Let's try again.

--- tests/test_test01.py
+++ tests/test_test01.py
@@ -23,7 +23,8 @@ def build(request):
     # for importing for the tests.  We need to change
     # easycython to use the begins CLI library to
     # provide a much nicer interface for cmd line args.
-    os.system('easycython test01.pyx > output.log')
+    os.system('easycython test01.pyx')
+    # os.system('python ../easycython/easycython.py test01.pyx > output.log')
     os.chdir('..')
     print('Done.')
 
@@ -35,12 +36,12 @@ def build(request):
     # This doesn't work because of the renaming in PEP 3149
     #expected_module = pth('test01' + ext)
     #assert os.path.exists(expected_module)
-    print('Current working dir: ' + os.getcwd())
-    print('Contents of the test folder: ' + '\n'.join(os.listdir('tests')))
-    if os.path.exists('tests/output.log'):
-        print('Contents of output.log: ' + '\n'.join(open('tests/output.log', 'r').readlines()))
-    else:
-        print('ERR: tests/output.log doesn''t exist.')
+    # print('Current working dir: ' + os.getcwd())
+    # print('Contents of the test folder: ' + '\n'.join(os.listdir('tests')))
+    # if os.path.exists('tests/output.log'):
+    #     print('Contents of output.log: ' + '\n'.join(open('tests/output.log', 'r').readlines()))
+    # else:
+    #     print('ERR: tests/output.log doesn''t exist.')
     assert os.path.exists(pth('test01.html'))
     assert os.path.exists(pth('test01.c'))
     assert os.path.exists(pth('build'))
