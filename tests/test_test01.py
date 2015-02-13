e57b3f421903b2e5053f35376004b0315b645d37
Caleb Hattingh <caleb.hattingh@gmail.com>
2015-02-13 10:50:32 +1000

    Trying to debug travis...

--- tests/test_test01.py
+++ tests/test_test01.py
@@ -23,7 +23,7 @@ def build(request):
     # for importing for the tests.  We need to change
     # easycython to use the begins CLI library to
     # provide a much nicer interface for cmd line args.
-    os.system('easycython test01.pyx')
+    os.system('easycython test01.pyx > output.log')
     os.chdir('..')
     print('Done.')
 
@@ -35,6 +35,12 @@ def build(request):
     # This doesn't work because of the renaming in PEP 3149
     #expected_module = pth('test01' + ext)
     #assert os.path.exists(expected_module)
+    print('Current working dir: ' + os.getcwd())
+    print('Contents of the test folder: ' + os.listdir('tests'))
+    if os.path.exists('tests/output.log'):
+        print('Contents of output.log: ' + '\n'.join(open('tests/output.log', 'r').readlines()))
+    else:
+        print('ERR: tests/output.log doesn''t exist.')
     assert os.path.exists(pth('test01.html'))
     assert os.path.exists(pth('test01.c'))
     assert os.path.exists(pth('build'))
