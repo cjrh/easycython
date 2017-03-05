80b926b153f76241747b39f60f52e16194f4ddbf
Caleb Hattingh <caleb.hattingh@gmail.com>
2017-03-05 21:30:10 +1000

    Change miniconda download path

--- appveyor/createenv.cmd
+++ appveyor/createenv.cmd
@@ -19,10 +19,10 @@ IF "%PLATFORM%"=="x64" (
     CALL "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /Release /x64
     SET DISTUTILS_USE_SDK=1
     SET MSSdk=1
-    SET MINICONDA=Miniconda-3.6.0-Windows-x86_64.exe
+    SET MINICONDA=Miniconda3-latest-Windows-x86_64.exe
 ) ELSE (
     CALL "C:\Program Files\Microsoft SDKs\Windows\%WINDOWS_SDK_VERSION%\Bin\SetEnv.cmd" /Release /x86
-    SET MINICONDA=Miniconda-3.6.0-Windows-x86.exe
+    SET MINICONDA=Miniconda3-latest-Windows-x86.exe
     REM TODO Ask continuum to use a "latest" URL for the most recent miniconda
 )
 ECHO Windows SDK enabled.
